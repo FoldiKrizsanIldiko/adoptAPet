fb8ac0fa42c57d0b8fd7e05a2be8c27a37d96489
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-07-31 15:37:23 +0200

    :sparkles: Create feature file for adopt with empty step definitions

---
+++ FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/AdoptAPetStepDefinitions.java
@@ -0,0 +1,52 @@
+package fki.adoptAPet.tests;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AdoptAPetStepDefinitions {
+    @Given("I am on adopt page")
+    public void iAmOnAdoptPage() {
+    }
+
+    @Given("I can see at least one adoptable pet i have not sign in for jet")
+    public void iCanSeeAtLeastOneAdoptablePetIHaveNotSignInForJet() {
+    }
+
+    @When("I click on I'd like to adopt button")
+    public void iClickOnIDLikeToAdoptButton() {
+    }
+
+    @Then("I can see a notification")
+    public void iCanSeeANotification() {
+    }
+
+    @And("I am redirected to My Application page")
+    public void iAmRedirectedToMyApplicationPage() {
+    }
+
+    @Given("I am on My Application page")
+    public void iAmOnMyApplicationPage() {
+    }
+
+    @Given("I have at least one application")
+    public void iHaveAtLeastOneApplication() {
+    }
+
+    @And("I am redirected to adopt page")
+    public void iAmRedirectedToAdoptPage() {
+    }
+
+    @Then("the I'd like to adopt button for the animal I am assigned on is disabled")
+    public void theIDLikeToAdoptButtonForTheAnimalIAmAssignedOnIsDisabled() {
+    }
+
+    @When("I click on enabled I'd like to adopt button")
+    public void iClickOnEnabledIDLikeToAdoptButton() {
+    }
+
+    @And("I have more than one application")
+    public void iHaveMoreThanOneApplication() {
+    }
+}
---
+++ FontendTests/adoptAPetFrontend/src/test/resources/fki.adoptAPet.tests/adoptAPet.feature
@@ -0,0 +1,27 @@
+@adopt
+Feature:
+  I want to adopt an animal
+
+
+  Scenario: I can sign up to adopt the pet
+    Given I am on adopt page
+    Given I can see at least one adoptable pet i have not sign in for jet
+    When I click on I'd like to adopt button
+    Then I can see a notification
+    And I am redirected to My Application page
+
+  Scenario: I can not sign up two times for the same pet
+    Given I am on My Application page
+    Given I have at least one application
+    When I click on Adoptable pets button
+    And I am redirected to adopt page
+    Then the I'd like to adopt button for the animal I am assigned on is disabled
+
+  Scenario: I can assign to more animal
+    Given I am on adopt page
+    Given I have at least one application
+    When I click on enabled I'd like to adopt button
+    Then I can see a notification
+    And I am redirected to My Application page
+    And I have more than one application
+
