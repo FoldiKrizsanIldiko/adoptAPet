2b52fd04b9be59e3d252aa4aa511d58471fabff6
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-07-31 15:37:48 +0200

    :sparkles: Create feature file for browse applications with empty step definitions

---
+++ FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/BrowseApplicationsStepDefinitions.java
@@ -0,0 +1,23 @@
+package fki.adoptAPet.tests;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BrowseApplicationsStepDefinitions {
+    @When("I click on My applications button")
+    public void iClickOnMyApplicationsButton() {
+    }
+
+    @Then("I can see only the pets I want to adapt or the message that I do not have applicaton")
+    public void iCanSeeOnlyThePetsIWantToAdaptOrTheMessageThatIDoNotHaveApplicaton() {
+    }
+
+    @And("I check out the {} checkbox")
+    public void iCheckOutTheCheckbox(String arg0) {
+    }
+
+    @Then("I can see only the {} applications or message that I do not have {} application")
+    public void iCanSeeOnlyTheApplicationsOrMessageThatIDoNotHaveApplication(String arg0, String arg1) {
+    }
+}
