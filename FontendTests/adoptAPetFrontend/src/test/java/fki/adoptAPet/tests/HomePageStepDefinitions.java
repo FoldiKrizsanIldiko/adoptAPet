eeba8a4919d838703692cd06e935ccfa7c1767a0
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-08-08 11:24:31 +0200

    :sparkles: create feature file for opening home page with empty step definitions

---
+++ FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/HomePageStepDefinitions.java
@@ -0,0 +1,19 @@
+package fki.adoptAPet.tests;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomePageStepDefinitions {
+    @When("I navigate to the home page")
+    public void iNavigateToTheHomePage() {
+    }
+
+    @Then("I can see navbar")
+    public void iCanSeeNavbar() {
+    }
+
+    @And("I can see welcome message")
+    public void iCanSeeWelcomeMessage() {
+    }
+}
---
+++ FontendTests/adoptAPetFrontend/src/test/resources/fki.adoptAPet.tests/openHomePage.feature
@@ -0,0 +1,9 @@
+@home
+Feature: The home page can be oened
+
+  Scenario:
+    When I navigate to the home page
+    Then I can see navbar
+    And I can see welcome message
+
+
