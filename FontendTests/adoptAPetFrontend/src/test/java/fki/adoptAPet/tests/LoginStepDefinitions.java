55dafafd1088f94e02fb482b3e24eead7c2ca78a
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-08-09 10:59:09 +0200

    :sparkles: create feature file for login with empty step definitions

---
+++ FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/LoginStepDefinitions.java
@@ -0,0 +1,31 @@
+package fki.adoptAPet.tests;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+    @When("I click on Login button")
+    public void iClickOnLoginButton() {
+    }
+
+    @Then("The login page is opening")
+    public void theLoginPageIsOpening() {
+    }
+
+    @When("I type in my valid email address")
+    public void iTypeInMyValidEmailAddress() {
+    }
+
+    @And("I type in my valid password")
+    public void iTypeInMyValidPassword() {
+    }
+
+    @Then("I should be logged in successfully")
+    public void iShouldBeLoggedInSuccessfully() {
+    }
+
+    @And("I should been redirected to page with adoptable pets")
+    public void iShouldBeenRedirectedToPageWithAdoptablePets() {
+    }
+}
