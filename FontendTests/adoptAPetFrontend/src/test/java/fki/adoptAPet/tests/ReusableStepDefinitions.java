2307af0416e507ec0c86622e8e10e0b4f1b8b0c4
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-08-13 18:11:11 +0200

    :sparkles: Move steps that are often used to a separate class.

---
+++ FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/ReusableStepDefinitions.java
@@ -0,0 +1,55 @@
+package fki.adoptAPet.tests;
+
+import fki.adoptAPet.PageFactory.HomePage;
+import fki.adoptAPet.PageFactory.LoginPage;
+import fki.adoptAPet.PageFactory.NavbarPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ReusableStepDefinitions {
+
+    private static WebDriver driver;
+
+    public static void setDriver(WebDriver webdriver){
+        driver=webdriver;
+    }
+
+    @Given("I am on home page")
+    public void iAmOnHomePage() {
+       new HomePage(driver).openHomePage();
+    }
+
+    @When("I am logged in")
+    public void iAmLoggedIn() {
+
+        new NavbarPage(driver).goToLoginPage();
+        new LoginPage(driver).inputCredentials();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Set<Cookie> cookies = driver.manage().getCookies();
+        assertFalse(cookies.isEmpty(), "There are no cookies");
+    }
+
+    @When("I am NOT logged in")
+    public void iAmNOTLoggedIn() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Set<Cookie> cookies =driver.manage().getCookies();
+        System.out.println(cookies + "iamNotLoggedIn");
+        assertTrue(cookies.isEmpty(), "There are some cookies stored in browser");
+    }
+
+}
