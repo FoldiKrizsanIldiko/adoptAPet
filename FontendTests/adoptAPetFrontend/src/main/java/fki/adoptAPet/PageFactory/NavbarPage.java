d8975128ef794d55a3946a2aedd08da111adf6b7
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-08-13 14:45:03 +0200

    :construction: displayed buttons

--- FontendTests/adoptAPetFrontend/src/main/java/fki/adoptAPet/PageFactory/NavbarPage.java
+++ FontendTests/adoptAPetFrontend/src/main/java/fki/adoptAPet/PageFactory/NavbarPage.java
@@ -14,9 +14,20 @@ public class NavbarPage {
     @FindBy(css = "[data-test='link-home']")
     WebElement HomeButton;
     @FindBy(css = "[data-test='link-adopt']")
-    WebElement Adoptable_pets;
+    WebElement Adoptable_petsButton;
     @FindBy(css = "[data-test='link-applications']")
-    WebElement My_applications;
+    WebElement My_applicationsButton;
+    @FindBy(css = "[data-test='link-login']")
+    WebElement LoginButton;
+    @FindBy(css = "[data-test='link-registration']")
+    WebElement RegistrationButton;
+    @FindBy(css = "[data-test='subsite-links']")
+    WebElement Navbar;
+    @FindBy(css = "[data-test='hello']")
+    WebElement HelloUserMessage;
+    @FindBy(css = "[data-test='link-logout']")
+    WebElement LogoutButton;
+
 
     public NavbarPage(WebDriver driver) {
         this.chromedriver = driver;
@@ -29,10 +40,46 @@ public class NavbarPage {
     }
 
     public void openAdoptable_petsPage() {
-        Adoptable_pets.click();
+        Adoptable_petsButton.click();
     }
 
     public void openMy_ApplicationsPage() {
-        My_applications.click();
+        My_applicationsButton.click();
+    }
+
+    public void navbar_is_present() {
+        Navbar.isDisplayed();
+    }
+
+    public boolean homeButtonIsVisible() {
+        return HomeButton.isEnabled();
+    }
+
+    public boolean adoptablePetsButtonIsVisible() {
+        return Adoptable_petsButton.isEnabled() && Adoptable_petsButton.isDisplayed();
+    }
+
+    public boolean loginButtonIsVisible() {
+        return LoginButton.isEnabled() && LoginButton.isDisplayed();
+    }
+
+    public boolean registrationButtonIsVisible() {
+        return RegistrationButton.isEnabled() && RegistrationButton.isDisplayed();
+    }
+
+    public boolean applicationButtonIsVisible() {
+        return My_applicationsButton.isEnabled() && My_applicationsButton.isDisplayed();
+    }
+
+    public boolean HelloUserIsVisible() {
+        return HelloUserMessage.isEnabled() && HelloUserMessage.isDisplayed();
+    }
+
+    public boolean LogoutButtonIsVisible() {
+        return LogoutButton.isEnabled() && LogoutButton.isDisplayed();
+    }
+
+    public void goToLoginPage() {
+        LoginButton.click();
     }
 }
