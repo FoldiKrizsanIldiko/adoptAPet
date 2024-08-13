676eed4ab5a1e66db5b393e400ed9b8d6f620d39
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-08-13 09:12:24 +0200

    :pencil2: better sentence

--- FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/BrowsePetsStepDefinitions.java
+++ FontendTests/adoptAPetFrontend/src/test/java/fki/adoptAPet/tests/BrowsePetsStepDefinitions.java
@@ -1,20 +1,33 @@
 package fki.adoptAPet.tests;
 
+import fki.adoptAPet.PageFactory.HomePage;
+import fki.adoptAPet.PageFactory.NavbarPage;
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowsePetsStepDefinitions {
+    WebDriver driver = new ChromeDriver();
+HomePage homePage= new HomePage(driver);
+NavbarPage navbarPage=new NavbarPage(driver);
+
+
     @Given("I am on home page")
-    public void iAmOnHomePage() {
+    public void iAmOnHomePage() {homePage.openHomePage();
     }
 
     @When("I click on Adoptable pets button")
     public void iClickOnAdoptablePetsButton() {
+        navbarPage.openAdoptable_petsPage();
     }
 
-    @Then("I can see if there are any pets for adopt")
-    public void iCanSeeIfThereAreAnyPetsForAdopt() {
+    @Then("I can see if there are any pets for adoption")
+    public void iCanSeeIfThereAreAnyPetsForAdoption() {
+
     }
 
     @When("I click on the selector")
@@ -25,7 +38,16 @@ public class BrowsePetsStepDefinitions {
     public void iChoose(String arg0) {
     }
 
-    @Then("I can see only {} types of animal or message that there is no animal of this type")
-    public void iCanSeeOnlyTypesOfAnimalOrMessageThatThereIsNoAnimalOfThisType(String arg0) {
+    @Then("I can see only {} types of animal or message that there are no animals of this type")
+    public void iCanSeeOnlyTypesOfAnimalOrMessageThatThereAreNoAnimalsOfThisType(String arg0) {
+    }
+    @After("@browsePets")
+    public void tearDown(){
+        driver.close();
+    }
+
+    @And("I am NOT logged in")
+    public void iAmNOTLoggedIn() {
+
     }
 }
--- FontendTests/adoptAPetFrontend/src/test/resources/fki.adoptAPet.tests/browsePets.feature
+++ FontendTests/adoptAPetFrontend/src/test/resources/fki.adoptAPet.tests/browsePets.feature
@@ -1,20 +1,23 @@
-@browse
+@browsePets
 Feature: Browse adoptable pets
-  I want to see if there are adoptable pets
+  I want to see if there are adoptable pets available
 
   Background:
     Given I am on home page
 
+
   Scenario: I can see the adoptable pets
+    And I am NOT logged in
     When I click on Adoptable pets button
-    Then I can see if there are any pets for adopt
+    Then I can see if there are any pets for adoption
 
 
     Scenario Outline: I can filter the pets
+      When I am NOT logged in
       When I click on Adoptable pets button
       When I click on the selector
       When I choose <option>
-      Then I can see only <option> types of animal or message that there is no animal of this type
+      Then I can see only <option> types of animal or message that there are no animals of this type
 
       Examples:
         | option    |
