b8e31f145373be4e767a844230558566f6c386c9
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-08-13 08:57:24 +0200

    :sparkles: create Adopt Page Pagefactory with 3 element

---
+++ FontendTests/adoptAPetFrontend/src/main/java/fki/adoptAPet/PageFactory/AdoptPage.java
@@ -0,0 +1,32 @@
+package fki.adoptAPet.PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AdoptPage {
+    WebDriver chromedriver;
+    WebDriverWait wait;
+
+    @FindBy(xpath = "//select[@name='speciesValue']/option")
+    List<WebElement> selectorOfSpecies;
+
+    @FindBy(xpath = "//ul[@data-test='adoptable-pets-list']/li")
+    List<WebElement> adoptablePets;
+
+    @FindBy(css = "[test-data='not-have-applications']")
+    WebElement message;
+    //You haven't handed in an application to adopt a pet yet.
+
+    public AdoptPage(WebDriver driver) {
+        this.chromedriver = driver;
+        PageFactory.initElements(driver, this);
+        chromedriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        wait = new WebDriverWait(chromedriver, Duration.ofSeconds(15));
+    }
+}
